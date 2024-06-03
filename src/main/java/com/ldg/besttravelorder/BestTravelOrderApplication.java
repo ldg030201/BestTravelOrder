4d285e1f2903353ac37e89fb671db8b153d9328a
ldg <ldg030201>
2024-06-03 23:10:23 +0900

    init

---
+++ src/main/java/com/ldg/besttravelorder/BestTravelOrderApplication.java
@@ -0,0 +1,13 @@
+package com.ldg.besttravelorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestTravelOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestTravelOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ldg/besttravelorder/BestTravelOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldg.besttravelorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestTravelOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
